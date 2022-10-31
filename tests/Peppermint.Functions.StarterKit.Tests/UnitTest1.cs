0dc8e21d6cc7427e8a7d97a8cb2ed6187d015e2d
Alexandre JULIEN <mail@alexandrejulien.fr>
2022-10-31 21:06:43 +0100

    Skeleton.

---
+++ tests/Peppermint.Functions.StarterKit.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Peppermint.Functions.StarterKit.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
